0004858325bb1a7067d23c6104ee1eabd98dcb35
Andriy Fedorchenko <doksel7@gmail.com>
2018-01-05 14:33:13 +0200

    Added loaders in webpack.config.js

--- news_site_react/webpack.config.js
+++ news_site_react/webpack.config.js
@@ -14,6 +14,48 @@ module.exports = {
         extensions:['.js','.jsx']
     },
     module:{
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: "babel",
+                exclude: [/node_modules/, /public/]
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader!autoprefixer-loader",
+                exclude: [/node_modules/, /public/]
+            },
+            {
+                test: /\.less$/,
+                loader: "style-loader!css-loader!autoprefixer-loader!less",
+                exclude: [/node_modules/, /public/]
+            },
+            {
+                test: /\.gif$/,
+                loader: "url-loader?limit=10000&mimetype=image/gif"
+            },
+            {
+                test: /\.jpg$/,
+                loader: "url-loader?limit=10000&mimetype=image/jpg"
+            },
+            {
+                test: /\.png$/,
+                loader: "url-loader?limit=10000&mimetype=image/png"
+            },
+            {
+                test: /\.svg/,
+                loader: "url-loader?limit=26000&mimetype=image/svg+xml"
+            },
+            {
+                test: /\.jsx$/,
+                loader: "react-hot!babel",
+                exclude: [/node_modules/, /public/]
+            },
+            {
+                test: /\.json$/,
+                loader: "json-loader"
+            }
+        ],
         rules:[   //загрузчик для jsx
             {
                 test: /\.jsx?$/, // определяем тип файлов
